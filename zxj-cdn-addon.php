a598631d1f821cb2bb427d987d1abfa25c52a3cc
jinger7281 <jinger7281@msn.cn>
2021-06-01 10:44:43 +0800

    initial commit

---
+++ zxj-cdn-addon.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: CDN附加组件
+ * Plugin URI: http://www.zhengxiaojing.cn
+ * Description: 将网站所有素材资源，样式、脚本、字体，图片上传到CDN中
+ * Version: 0.0.1
+ * Requires PHP: 7.4
+ * Author: jinger7281
+ * License: Apache License V2.0
+ * License URI: https://www.apache.org/licenses/LICENSE-2.0
+ */
+namespace zxj\cdn;
