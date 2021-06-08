4cccd37062a3d649063c3e980a7f188ffa6ba78a
jinger7281 <jinger7281@msn.cn>
2021-06-08 15:00:51 +0800

    修改 设置页面样式

--- templates/setting-page.php
+++ templates/setting-page.php
@@ -1,8 +1,8 @@
 <script src="https://cdn.bootcdn.net/ajax/libs/angular.js/1.7.8/angular.min.js"></script>
 <script>
-var plg_uri = '<?php echo $url; ?>';
+    var plg_uri = '<?php echo $url; ?>';
 </script>
-<script src="<?php echo $url.'assets/js/main.js'; ?>"></script>
+<script src="<?php echo $url . 'assets/js/main.js'; ?>"></script>
 <style>
     .item {
         margin: 5px;
@@ -13,22 +13,26 @@ var plg_uri = '<?php echo $url; ?>';
     <div ng-controller="main">
         <label>CDN服务提供商：</label>
         <select ng-model="cdnType" ng-options="item for item in cdnTypes" ng-init="cdnType='七牛'"></select>
-        <table class="for-table" role="presentation">
+        <table class="form-table" role="presentation">
+            <tr>
+                <th>CDN加速域名(url): </th>
+                <td><input type="text" ng-model="f.url" placeholder="CDN URL" class="regular-text"></td>
+            </tr>
+            <tr>
+                <th>存储空间名称(Bucket): </th>
+                <td><input type="text" ng-model="f.bucket" placeholder="Bucket" class="regular-text"></td>
+            </tr>
             <tr>
                 <th>AppKey: </th>
-                <td><input type="text" ng-model="f.appKey" placeholder="AppKey"></td>
+                <td><input type="password" ng-model="f.appKey" placeholder="AppKey" class="regular-text strong"></td>
             </tr>
             <tr>
                 <th>AppSecret: </th>
-                <td><input type="text" ng-model="f.appSecret" placeholder="AppSecret"></td>
+                <td><input type="password" ng-model="f.appSecret" placeholder="AppSecret" class="regular-text strong"></td>
             </tr>
             <tr>
-                <th>Bucket: </th>
-                <td><input type="text" ng-model="f.bucket" placeholder="Bucket"></td>
-            </tr>
-            <tr>
-                <th>EndPoint: </th>
-                <td><input type="text" ng-model="f.endPoint" placeholder="EndPoint"></td>
+                <th>节点名称(EndPoint): </th>
+                <td><input type="text" ng-model="f.endPoint" placeholder="例如：z1(七牛)/cn-hangzhou(阿里云)" class="regular-text"></td>
             </tr>
         </table>
         <p class="submit">
